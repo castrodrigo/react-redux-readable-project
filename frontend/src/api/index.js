37b863d55ae4c188536a768b47829c96f8523d3b
castrodrigo <castrodrigo@yahoo.com.br>
2019-02-22 23:46:39 +0100

    Map all the endpoints that are supported in the API

---
+++ frontend/src/api/index.js
@@ -0,0 +1,41 @@
+// get all Categories
+// GET /categories
+
+// get all Posts
+// GET /posts
+
+// get all Posts from Category
+// GET /:category/posts
+
+// get a Post
+// GET /posts/:id
+
+// add a Post
+// POST /posts
+
+// update a post
+// PUT /posts/:id
+
+// vote a post
+// POST /posts/:id
+
+// delete a post
+// DELETE /posts/:id
+
+// get all Comments from Post
+// GET /posts/:id/comments
+
+// add a comment
+// POST /comments
+
+// get a comment
+// GET /comments/:id
+
+// vote a comment
+// POST /comments/:id
+
+// update a comment
+// PUT /comments/:id
+
+// delete a comment
+// DELETE /comments/:id
