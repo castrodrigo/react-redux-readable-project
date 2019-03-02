cfdbaa66717855aaad9d4e862bb34a2a49f20100
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-03 00:06:31 +0100

    Add reducer and action creators to fetch categories data

--- frontend/src/reducers/categories.js
+++ frontend/src/reducers/categories.js
@@ -1,5 +1,16 @@
+import { GET_CATEGORIES } from "../actions/categories";
+
 export default (state = {}, action) => {
   switch (action.type) {
+    case GET_CATEGORIES:
+      const categoryObject = {};
+      action.categories.map(
+        category => (categoryObject[category.path] = category)
+      );
+      return {
+        ...state,
+        ...categoryObject
+      };
     default:
       return state;
   }
