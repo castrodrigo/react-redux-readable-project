e2fa504e9e6fd60d0522b9d843b9953fcd478459
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-02 22:21:49 +0100

    Create basic dummy comment component

--- frontend/src/components/Comment/index.js
+++ frontend/src/components/Comment/index.js
@@ -1,5 +1,22 @@
 import React from "react";
 
-const Comment = () => <div>Comment</div>;
+const Comment = ({
+  comment: {
+    id,
+    parentId,
+    timestamp,
+    body,
+    author,
+    voteScore,
+    deleted,
+    parentDeleted
+  }
+}) => (
+  <div>
+    <span>{timestamp}</span> by <span>{author}</span>
+    <div>{body}</div>
+    <section>score: {voteScore}</section>
+  </div>
+);
 
 export default Comment;
