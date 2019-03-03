02d31c249ffc855f1693008d02fe4df54c66cf38
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-03 12:42:45 +0100

    Add custom layout for comment list

--- frontend/src/components/Comment/index.js
+++ frontend/src/components/Comment/index.js
@@ -1,4 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+const CommentWrapper = styled.div`
+  background: #d1ccce;
+  padding: 0.75em;
+  color: #001936;
+`;
+
+const DetailSection = styled.section`
+  font-size: 0.75em;
+  > span,
+  > button {
+    font-weight: bold;
+  }
+  > button {
+    color: #001936;
+    border: 0;
+    padding: 0;
+    cursor: pointer;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  margin: 20px 0;
+`;
 
 const Comment = ({
   comment: {
@@ -12,11 +37,13 @@ const Comment = ({
     parentDeleted
   }
 }) => (
-  <div>
-    <span>{timestamp}</span> by <span>{author}</span>
-    <div>{body}</div>
+  <CommentWrapper>
+    <DetailSection>
+      Commented in <span>{timestamp}</span> by <span>{author}</span>
+    </DetailSection>
+    <ContentWrapper>{body}</ContentWrapper>
     <section>score: {voteScore}</section>
-  </div>
+  </CommentWrapper>
 );
 
 export default Comment;
