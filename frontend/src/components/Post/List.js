bd50f2f660bfee22c7dbd106d2ade73a0cf67df9
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-03 11:56:21 +0100

    Add basic styling for Post List Item

--- frontend/src/components/Post/List.js
+++ frontend/src/components/Post/List.js
@@ -1,14 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 import Post from "../../containers/Post/Post";
+
+const ListItem = styled.li`
+  margin-bottom: 0.75em;
+`;
 
 const List = ({ title, postIds }) => (
   <div>
     <h3>{title}</h3>
     <ul>
       {postIds.map(id => (
-        <li key={id}>
+        <ListItem key={id}>
           <Post id={id} />
-        </li>
+        </ListItem>
       ))}
     </ul>
   </div>
--- frontend/src/components/Post/index.js
+++ frontend/src/components/Post/index.js
@@ -1,5 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { FaComments } from "react-icons/fa";
+import { formatTimestamp } from "../../util/date";
+
+const PostWrapper = styled.div`
+  border: 1px solid #d1ccce;
+  padding: 0.75em;
+  color: #001936;
+`;
+
+const Title = styled.h2`
+  margin: 0 0 4px;
+`;
+
+const DetailSection = styled.section`
+  font-size: 0.75em;
+  color: #8e8b8c;
+  > span,
+  > a {
+    font-weight: bold;
+  }
+  > a {
+    color: #001936;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  margin: 20px 0;
+`;
+
+const CommentSection = styled.section`
+  border-top: 1px solid #d1ccce;
+  padding-top: 0.75em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const Post = ({
   post: {
@@ -14,12 +51,20 @@ const Post = ({
   }
 }) => (
   <Link to={`/${category}/${id}`}>
-    <h2>{title}</h2>
-    <span>{category}</span> | <span>{timestamp}</span> | <span>{author}</span>
-    <div>{body}</div>
-    <section>
-      ({commentCount}) comments | score: {voteScore}
-    </section>
+    <PostWrapper>
+      <Title>{title}</Title>
+      <DetailSection>
+        Published in <span>{formatTimestamp(timestamp)}</span> by{" "}
+        <span>{author}</span>, in <Link to={`/${category}`}>{category}</Link>
+      </DetailSection>
+      <ContentWrapper>{body}</ContentWrapper>
+      <CommentSection>
+        <span>
+          <FaComments /> ({commentCount})
+        </span>
+        <span>score: {voteScore}</span>
+      </CommentSection>
+    </PostWrapper>
   </Link>
 );
 
