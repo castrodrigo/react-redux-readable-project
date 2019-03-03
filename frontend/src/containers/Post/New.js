af1ec79ea7460840c86a69a18ba92c4b9ccaee64
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-03 10:42:11 +0100

    Add new post component to start make routes work

---
+++ frontend/src/containers/Post/New.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PostNew from "../../components/Post/New";
+
+class NewContainer extends React.Component {
+  render() {
+    return <PostNew />;
+  }
+}
+
+export default NewContainer;
