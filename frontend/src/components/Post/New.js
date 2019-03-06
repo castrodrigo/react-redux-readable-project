ede063acc4e03c8e368d3bdb38eb3649a697678b
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-07 00:49:20 +0100

    Make dummy component a controlled one for form handling

--- frontend/src/components/Post/New.js
+++ frontend/src/components/Post/New.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Select from "react-styled-select";
+import PropTypes from "prop-types";
+import Select from "react-select";
 import styled from "styled-components";
 import Layout from "../Layout";
 
@@ -15,7 +16,7 @@ const Input = styled.input`
   padding: 0.75em;
   width: 70%;
   border: 1px solid #012542;
-  border-radius: 2px;
+  border-radius: 4px;
   box-sizing: border-box;
 `;
 
@@ -23,16 +24,21 @@ const Textarea = styled.textarea`
   padding: 0.75em;
   width: 70%;
   border: 1px solid #012542;
-  border-radius: 2px;
+  border-radius: 4px;
   box-sizing: border-box;
 `;
 
 const FormSelect = styled(Select)`
   width: 70%;
-  border: 1px solid #012542;
-  border-radius: 2px;
   box-sizing: border-box;
   font-size: 16px;
+  &:focus {
+    outline: none;
+  }
+  & > div,
+  & > div:hover {
+    border-color: #012542;
+  }
 `;
 
 const Label = styled.label`
@@ -46,40 +52,100 @@ const Label = styled.label`
 
 const Button = styled.button`
   border: 1px solid #ffffff;
-  border-radius: 2px;
+  border-radius: 4px;
   color: #fff;
   background: #840032;
   padding: 10px;
   margin-top: 10px;
+  &:disabled {
+    background: #f0f0f0;
+  }
 `;
 
-const New = () => (
-  <Layout
-    content={
-      <React.Fragment>
-        <h2>Create a new post</h2>
-        <FormContainer>
-          <Label>
-            Title:
-            <Input type="text" name="name" />
-          </Label>
-          <Label>
-            Category:
-            <FormSelect name="category" options={[]} />
-          </Label>
-          <Label>
-            Body:
-            <Textarea rows="10" name="body" />
-          </Label>
-          <Label>
-            Author:
-            <Input type="text" name="body" />
-          </Label>
-          <Button>Send</Button>
-        </FormContainer>
-      </React.Fragment>
-    }
-  />
-);
+class New extends React.Component {
+  state = {
+    title: "",
+    author: "",
+    category: "",
+    body: ""
+  };
+
+  handleOnChange = event => {
+    const {
+      target: { name, value }
+    } = event;
+    this.setState({ [name]: value });
+  };
+
+  handleOnSelect = category => {
+    this.setState({ category });
+  };
+
+  handleValidation = () => {
+    const { title, author, category, body } = this.state;
+    return title === "" || author === "" || category === "" || body === ""
+      ? false
+      : true;
+  };
+
+  render() {
+    return (
+      <Layout
+        content={
+          <React.Fragment>
+            <h2>Create a new post</h2>
+            <FormContainer onSubmit={this.props.onSubmit}>
+              <Label>
+                Title:
+                <Input
+                  type="text"
+                  name="title"
+                  onChange={this.handleOnChange}
+                  value={this.state.title}
+                />
+              </Label>
+              <Label>
+                Category:
+                <FormSelect
+                  name="category"
+                  options={[
+                    { label: "Test", value: 1 },
+                    { label: "T2est", value: 2 }
+                  ]}
+                  onChange={this.handleOnSelect}
+                  searchable={false}
+                  value={this.state.category}
+                />
+              </Label>
+              <Label>
+                Body:
+                <Textarea
+                  rows="10"
+                  name="body"
+                  onChange={this.handleOnChange}
+                  value={this.state.body}
+                />
+              </Label>
+              <Label>
+                Author:
+                <Input
+                  type="text"
+                  name="author"
+                  onChange={this.handleOnChange}
+                  value={this.state.author}
+                />
+              </Label>
+              <Button disabled={!this.handleValidation()}>Send</Button>
+            </FormContainer>
+          </React.Fragment>
+        }
+      />
+    );
+  }
+}
+
+New.propTypes = {
+  onSubmit: PropTypes.func.isRequired
+};
 
 export default New;
--- frontend/src/containers/Post/New.js
+++ frontend/src/containers/Post/New.js
@@ -2,8 +2,12 @@ import React from "react";
 import PostNew from "../../components/Post/New";
 
 class NewContainer extends React.Component {
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
-    return <PostNew />;
+    return <PostNew onSubmit={this.handleSubmit} />;
   }
 }
 
