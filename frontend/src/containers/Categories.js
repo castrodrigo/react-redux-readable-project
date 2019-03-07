6a7c192ddf4ec5aa01287cc6384dd115d04e651a
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-08 00:12:41 +0100

    Fetch data from API on the App loader instead categories

--- frontend/src/containers/Categories.js
+++ frontend/src/containers/Categories.js
@@ -1,22 +1,9 @@
-import React from "react";
 import { connect } from "react-redux";
-import { handleGetCategories } from "../actions/categories";
 import CategoryList from "../components/Category/List";
 
-class Categories extends React.Component {
-  componentDidMount() {
-    this.props.dispatch(handleGetCategories());
-  }
-
-  render() {
-    return this.props.categories.length > 0 ? (
-      <CategoryList title="Categories" categories={this.props.categories} />
-    ) : null;
-  }
-}
-
 const mapStateToProps = ({ categories }) => ({
+  title: "Categories",
   categories: Object.keys(categories)
 });
 
-export default connect(mapStateToProps)(Categories);
+export default connect(mapStateToProps)(CategoryList);
