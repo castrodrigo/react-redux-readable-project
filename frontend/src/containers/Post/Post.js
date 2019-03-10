33bca8976affa4e86ee814217743ce9eba973934
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-10 19:08:48 +0100

    Add dispatch vote functions

--- frontend/src/containers/Post/Post.js
+++ frontend/src/containers/Post/Post.js
@@ -1,8 +1,26 @@
 import { connect } from "react-redux";
+import { handleVotePost, UPVOTE, DOWNVOTE } from "../../actions/posts";
 import Post from "../../components/Post";
 
 const mapStateToProps = ({ posts }, { id }) => ({
   post: posts[id] || null
 });
 
-export default connect(mapStateToProps)(Post);
+const mapDispatchToProps = dispatch => ({
+  voteUp: id => dispatch(handleVotePost(id, UPVOTE)),
+  voteDown: id => dispatch(handleVotePost(id, DOWNVOTE))
+});
+
+const mergeProps = ({ post }, { voteUp, voteDown }, { id }) => {
+  return {
+    post,
+    voteUp: () => voteUp(id),
+    voteDown: () => voteDown(id)
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
+)(Post);
