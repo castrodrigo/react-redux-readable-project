2e1332ece831c18cfec3b84e57c4c133dcaffd67
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-11 00:46:15 +0100

    Dispatch get post after success on add comment

--- frontend/src/containers/Comment/New.js
+++ frontend/src/containers/Comment/New.js
@@ -1,11 +1,14 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { handleAddComment } from "../../actions/comments";
+import { handleGetPost } from "../../actions/posts";
 import CommentNew from "../../components/Comment/New";
 
 const maptDispatchToProps = dispatch => ({
   submitComment: (comment, postId) =>
-    dispatch(handleAddComment(comment, postId))
+    dispatch(handleAddComment(comment, postId)).then(
+      dispatch(handleGetPost(postId))
+    )
 });
 
 const mergeProps = (propsFromState, { submitComment }, { postId }) => ({
