3d7483b724569cc54159d926c80cfb10dc70fdf1
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-10 23:55:57 +0100

    Add action creator to handle comment by post

--- frontend/src/actions/comments.js
+++ frontend/src/actions/comments.js
@@ -1,3 +1,8 @@
+import { showLoading, hideLoading } from "react-redux-loading";
+import * as api from "../api";
+import { generateUUID } from "../util/id";
+import { generateTimestamp } from "../util/date";
+
 export const GET_COMMENTS = "GET_COMMENTS";
 export const ADD_COMMENT = "ADD_COMMENT";
 export const UPDATE_COMMENT = "UPDATE_COMMENT";
@@ -9,3 +14,22 @@ export const getComments = (comments, postId) => ({
   postId,
   comments
 });
+
+const addComment = comment => ({
+  type: ADD_COMMENT,
+  comment
+});
+
+export const handleAddComment = (comment, postId) => dispatch => {
+  dispatch(showLoading());
+
+  return api
+    .addComment({
+      id: generateUUID(),
+      ...comment,
+      timestamp: generateTimestamp(),
+      parentId: postId
+    })
+    .then(comment => dispatch(addComment(comment)))
+    .then(() => dispatch(hideLoading()));
+};
--- frontend/src/components/Comment/List.js
+++ frontend/src/components/Comment/List.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import Comment from "../../containers/Comment";
+import Comment from "../../containers/Comment/Comment";
 
 const ListItem = styled.li`
   margin-bottom: 0.75em;
