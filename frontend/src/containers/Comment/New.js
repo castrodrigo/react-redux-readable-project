5650148aecd8761a31d7d55c89345e2ea1d9b340
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-10 23:56:59 +0100

    Add dispatcher on container to handle new comment

--- frontend/src/containers/Comment/New.js
+++ frontend/src/containers/Comment/New.js
@@ -1,13 +1,15 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import { handleAddComment } from "../../actions/comments";
 import CommentNew from "../../components/Comment/New";
 
 const maptDispatchToProps = dispatch => ({
-  submitComment: (postId, comment) => console.log(postId, comment)
+  submitComment: (comment, postId) =>
+    dispatch(handleAddComment(comment, postId))
 });
 
 const mergeProps = (propsFromState, { submitComment }, { postId }) => ({
-  onSubmit: data => submitComment(postId, data)
+  onSubmit: data => submitComment(data, postId)
 });
 
 export default withRouter(
