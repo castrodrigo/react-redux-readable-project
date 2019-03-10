bab1e829a3f3f77071f710a4fc0e9724b9bcf05a
castrodrigo <castrodrigo@yahoo.com.br>
2019-03-10 15:49:54 +0100

    Add action creator and thunk handlers to update

--- frontend/src/containers/Post/Edit.js
+++ frontend/src/containers/Post/Edit.js
@@ -1,30 +1,31 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import { handleAddPost } from "../../actions/posts";
+import { handleUpdatePost } from "../../actions/posts";
 import PostEdit from "../../components/Post/Edit";
 
 const mapStateToProps = ({ posts, categories }, props) => {
   const { id } = props.match.params;
   return {
+    id,
     post: posts[id] || null,
     categories
   };
 };
 
 const maptDispatchToProps = dispatch => ({
-  submitPost: post =>
-    dispatch(handleAddPost(post)).then(() =>
-      this.props.history.push(`/${post.category}/`)
+  submitPost: (history, id, post) =>
+    dispatch(handleUpdatePost(id, post)).then(() =>
+      history.push(`/${post.category}/${id}`)
     )
 });
 
-const mergeProps = ({ post, categories }, { submitPost }) => ({
+const mergeProps = ({ id, post, categories }, { submitPost }, { history }) => ({
   post,
   categories: Object.keys(categories).map(key => ({
     value: key,
     label: categories[key].name
   })),
-  onSubmit: submitPost
+  onSubmit: data => submitPost(history, id, data)
 });
 
 export default connect(
